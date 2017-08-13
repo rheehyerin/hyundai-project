94d2bd800fefdb750557aebbda76cfb99c418e87
rheehyerin <toohappy2122@gmail.com>
2017-08-14 08:46:22 +0900

    qnsrl

--- pickmeup/settings/prod.py
+++ pickmeup/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "staticfiles")
-MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
+
 
 DATABASES = {
     'default': {
